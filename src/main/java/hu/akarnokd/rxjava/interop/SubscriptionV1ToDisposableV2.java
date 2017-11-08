691aae74487542ddc6352d0066b0f8d979b98c50
akarnokd <akarnokd@gmail.com>
2017-11-08 09:41:09 +0100

    Add header and short description to Artem's contr.

--- src/main/java/hu/akarnokd/rxjava/interop/SubscriptionV1ToDisposableV2.java
+++ src/main/java/hu/akarnokd/rxjava/interop/SubscriptionV1ToDisposableV2.java
@@ -1,8 +1,29 @@
+/*
+ * Copyright 2016 David Karnok
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package hu.akarnokd.rxjava.interop;
 
 import io.reactivex.disposables.Disposable;
 import rx.Subscription;
 
+/**
+ * Wraps a v1 Subscription and exposes it as a v2 Disposable.
+ * @since 0.11.0
+ * @author Artem Zinnatulin
+ */
 final class SubscriptionV1ToDisposableV2 implements Disposable {
 
     private final Subscription subscription;
